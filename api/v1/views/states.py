21341a83c0ab108100135e510aa447cbd615250e
Apam John Hyder <117967310+ApamJohnHyder@users.noreply.github.com>
2023-08-01 13:07:54 +0000

    states.py

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,28 +1,67 @@
-Create a new view for State objects that handles all default RESTFul API actions:
+#!/usr/bin/python3
+'''Contains the states view for the API.'''
+from flask import abort, jsonify, make_response, request
+from api.v1.views import app_views
+from models import storage
+from models.state import State
 
-In the file api/v1/views/states.py
-You must use to_dict() to retrieve an object into a valid JSON
-Update api/v1/views/__init__.py to import this new file
-Retrieves the list of all State objects: GET /api/v1/states
 
-Retrieves a State object: GET /api/v1/states/<state_id>
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def state():
+    """Retrieves the list of all State objects"""
+    objs = storage.all(State)
+    return jsonify([obj.to_dict() for obj in objs.values()])
 
-If the state_id is not linked to any State object, raise a 404 error
-Deletes a State object:: DELETE /api/v1/states/<state_id>
 
-If the state_id is not linked to any State object, raise a 404 error
-Returns an empty dictionary with the status code 200
-Creates a State: POST /api/v1/states
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def single_state(state_id):
+    """Retrieves a State object"""
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    return jsonify(obj.to_dict())
 
-You must use request.get_json from Flask to transform the HTTP body request to a dictionary
-If the HTTP body request is not valid JSON, raise a 400 error with the message Not a JSON
-If the dictionary doesn’t contain the key name, raise a 400 error with the message Missing name
-Returns the new State with the status code 201
-Updates a State object: PUT /api/v1/states/<state_id>
 
-If the state_id is not linked to any State object, raise a 404 error
-You must use request.get_json from Flask to transform the HTTP body request to a dictionary
-If the HTTP body request is not valid JSON, raise a 400 error with the message Not a JSON
-Update the State object with all key-value pairs of the dictionary.
-Ignore keys: id, created_at and updated_at
-Returns the State object with the status code 200
+@app_views.route('/states/<state_id>',
+                 methods=['DELETE'], strict_slashes=False)
+def del_state(state_id):
+    """Deletes a State object"""
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+    obj.delete()
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def post_state():
+    """Returns the new State with the status code 201"""
+    new_obj = request.get_json()
+    if not new_obj:
+        abort(400, "Not a JSON")
+    if 'name' not in new_obj:
+        abort(400, "Missing name")
+    obj = State(**new_obj)
+    storage.new(obj)
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 201)
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def put_state(state_id):
+    """ Updates a State object """
+    obj = storage.get(State, state_id)
+    if not obj:
+        abort(404)
+
+    req = request.get_json()
+    if not req:
+        abort(400, "Not a JSON")
+
+    for k, v in req.items():
+        if k not in ['id', 'created_at', 'updated_at']:
+            setattr(obj, k, v)
+
+    storage.save()
+    return make_response(jsonify(obj.to_dict()), 200)
